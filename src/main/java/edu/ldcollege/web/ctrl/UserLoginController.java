efd3817d6b0644b5b2cbc440b96d250fcf975e2b
sukeqiang <sukeqiang@163.com>
2017-02-28 01:47:06 +0800

    modify

--- src/main/java/edu/ldcollege/web/ctrl/UserLoginController.java
+++ src/main/java/edu/ldcollege/web/ctrl/UserLoginController.java
@@ -2,6 +2,9 @@ package edu.ldcollege.web.ctrl;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserLoginController {
@@ -9,5 +12,11 @@ public class UserLoginController {
 	@RequestMapping("/")
 	public String viewLogin() {
 		return "login";
+	}
+	
+	@RequestMapping(value = "/signin",method = RequestMethod.POST)
+	public @ResponseBody String signin(@RequestParam("login") String login,
+			@RequestParam("password") String password) {
+		return "{\"is_error\":null,\"is_reload_page\":null,\"new_location\":null,\"msgs\":[{\"body\":\"Login required\",\"type\":\"3\"}]}";
 	}
 }
