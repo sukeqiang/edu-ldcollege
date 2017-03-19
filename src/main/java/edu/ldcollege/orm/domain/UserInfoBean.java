c9f01ce9b6d3daedbdc35bb8e52c23df2a1f547c
sukeqiang <sukeqiang@163.com>
2017-03-20 00:16:36 +0800

    modify

---
+++ src/main/java/edu/ldcollege/orm/domain/UserInfoBean.java
@@ -0,0 +1,20 @@
+package edu.ldcollege.orm.domain;
+
+public class UserInfoBean {
+
+	private int age;
+	private String name;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
